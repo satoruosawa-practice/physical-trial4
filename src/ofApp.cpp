338dd6c0d13a107e969afc96533c92d29b7db4f6
Satoru Osawa <s.osawa29@gmail.com>
2017-06-17 12:29:07 +0900

    key control

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -2,9 +2,13 @@
 
 //--------------------------------------------------------------
 void ofApp::setup(){
+  //  ofSetFrameRate(60);
+  ofSetVerticalSync(false);
+  ofSetFrameRate(0);
+  ofBackground(63);
+  
   position_ = ofVec2f(100.0, ofGetHeight() * 0.5);
-  velocity_ = ofVec2f(1, 0.0) * PX_PER_METER;
-  acceleration_ = ofVec2f(1, 0.0) * PX_PER_METER;
+  velocity_ = ofVec2f(0.0, 0.0) * PX_PER_METER;
 
   app_time_.setup();
 }
@@ -20,10 +24,32 @@ void ofApp::update(){
 void ofApp::draw(){
   ofDrawGrid(100, 11, false, false, false, true);
   ofDrawCircle(position_, 10);
+  std::cout<<floor(0.1)<<std::endl;
+
+  ofDrawBitmapString("frameRate = " + ofToString(ofGetFrameRate()), 10, 20);
+  ofDrawBitmapString("acceleration = " + ofToString(acceleration_), 10, 40);
+  ofDrawBitmapString("speed = " + ofToString(velocity_), 10, 60);
+  ofDrawBitmapString("position = " + ofToString(position_), 10, 80);
 }
 
 //--------------------------------------------------------------
 void ofApp::keyPressed(int key){
+  switch(key) {
+    case OF_KEY_LEFT:
+      acceleration_ += ofVec2f(-1.0, 0.0) * PX_PER_METER;
+      break;
+    case OF_KEY_RIGHT:
+      acceleration_ += ofVec2f(1.0, 0.0) * PX_PER_METER;
+      break;
+    case OF_KEY_UP:
+      acceleration_ += ofVec2f(0.0, -1.0) * PX_PER_METER;
+      break;
+    case OF_KEY_DOWN:
+      acceleration_ += ofVec2f(0.0, 1.0) * PX_PER_METER;
+      break;
+    default:
+      break;
+  }
 }
 
 //--------------------------------------------------------------
